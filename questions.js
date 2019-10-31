8adaf7a302a918e2f0ca82ee7764b79a6cb88a4f
Belguith Omar <omar.belguith@medtech.tn>
2019-10-31 13:34:53 +0100

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,112 @@
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// Try these questions with your pair.
+// Discuss what the output would be
+// It is very important to discuss the output
+// It is very important to guess before trying the code on the console.
+// Try to guess the output it is for your own good
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+function one(){
+  for(var i = 0; i < 3; i++){
+    setTimeout(function(){
+      console.log(i);
+    }, 0)
+  }
+}
+
+//one();
+//Expected log:
+//Actual log:
+//Why does it log that?¿
+
+
+function two(){
+  for(let i = 0; i < 3; i++){
+    setTimeout(function(){
+      console.log(i);
+    }, 0)
+  }
+}
+
+//two();
+//Expected log:
+//Actual log:
+//Why does it log that?¿
+
+
+function three(){
+  for(var i = 0; i < 3; i++){
+    setTimeout(()=>(
+      console.log(i)
+    ), 0)
+  }
+}
+
+//three();
+//Expected log:
+//Actual log:
+//Why does it log that?¿
+
+
+function four(){
+  for(var i = 0; i < 3; i++){
+    setTimeout(function(){
+      var locali = i;
+      return function(){console.log(locali)}
+    }(), 0)
+  }
+}
+
+//four();
+//Expected log:
+//Actual log:
+//Why does it log that?¿
+
+
+function five(){
+  for(var i = 0; i < 3; i++){
+    setTimeout(function(locali){
+      return function(){console.log(locali)}
+    }(i), 0)
+  }
+}
+
+//five();
+//Expected log:
+//Actual log:
+//Why does it log that?
+
+
+function six(){
+  for(var i = 0; i < 3; i++){
+    setTimeout(function(locali){
+      var locali = i;
+      return function(){console.log(locali)}
+    }, 0)
+  }
+}
+
+//six();
+//Expected log:
+//Actual log:
+//Why does it log that?
+
+function seven(){
+  (function() {
+      const numbers = [0.1, 0.2, 0.3];
+      let sum = 0;
+
+      for (var i = 0; i < numbers.length; ++i) {
+          setTimeout(() => {
+              sum += numbers[i];
+          }, 0);
+      }
+
+      console.log(sum === 0.6);
+  })();
+}
+
+//seven();
+//Expected log:
+//Actual log:
+//Why does it log that?
